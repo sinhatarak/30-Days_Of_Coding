59fd5454c9c05e3d82cb1fdede59162a7ac5edc2
taraksinha <>
2021-10-11 13:49:27 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+'''
+https://www.teclado.com/30-days-of-python/python-30-day-1-exercise-solutions
+
+#1) Print your age to the console.
+# print("36")
+# 2) Calculate and print the number of days, weeks, and months in 27 years.
+print(27 * 365, "Days in 27 Years")
+print(27 * 52, "Weeks in 27 Years")
+print(27 * 12, "Months in 27 Years")
+'''
+#3) Calculate and print the area of a circle with a radius of 5 units.
+
+print(3.141 ** 5)
+
+
